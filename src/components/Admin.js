7d7e7840d9b19d365f4c92667b49ab27a6aee93e
kazi <champlonremii@gmail.com>
2022-04-21 15:51:57 +0200

    list / add / delete adresses

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -7,20 +7,57 @@ function Admin() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [logged, setLogged] = useState(false);
+    const [data, setData] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    const [address, setAddress] = useState('');
 
     useEffect(() => {
+        setLoaded(true)
+    },[data])
 
-    },[])
-
-    function loggin()
+    function login()
     {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then((userCredential) => {
                 setLogged(true);
                 console.log('login successful');
+                getData();
             }).catch((error) => {
                 console.log('login fail');
         })
+    }
+
+    function getData() {
+
+        ref.onSnapshot((querySnapShot) => {
+            const items = [];
+            querySnapShot.forEach((doc) => {
+                items.push(doc.data());
+            });
+            setData(items)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+    function deleteAddress(e) {
+        ref.doc(e.target.value).delete()
+    }
+
+    function addOnWhitelist()
+    {
+        let balance = 0;
+        let id = uuidv4();
+
+        let object = {address: address, balance: balance, id: id}
+
+        ref.doc(object.id).set(object)
+            .then(result => {
+                console.log('successful added');
+            }).catch((err) => {
+            console.log('error');
+        })
+        getData();
     }
 
     return (
@@ -29,15 +66,30 @@ function Admin() {
           {!logged
               ?
               <div>
-                  Loggin :<br />
+                  Login :<br />
                   <input type="email" placeholder="email" onChange={e => setEmail(e.target.value)} />
                   <br />
                   <input type="password" placeholder="password" onChange={e => setPassword(e.target.value)}/>
                   <br />
-                  <button onClick={loggin}>Loggin</button>
+                  <button onClick={login}>Login</button>
               </div>
               :
-              <div>PANEL ADMIN</div>
+              <div>
+                  PANEL ADMIN
+                  <br /><br />
+                  listing of addresses in whitelist
+                  {
+                      loaded &&
+                          data.map(elem => {
+                              return <li key={elem.id}>{elem.address} | {elem.balance} eth <button onClick={deleteAddress} value={elem.id}>delete</button> </li>
+                          })
+                  }
+                  <br /><br />
+                  Add address on whitelist
+                  <input type="text" onChange={e => setAddress(e.target.value)} />
+                  <button onClick={addOnWhitelist}>Add on whitelist</button>
+
+              </div>
           }
       </div>
     );
