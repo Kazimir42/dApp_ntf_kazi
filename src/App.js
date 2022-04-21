f8767a5bd1a6ec75a9649b1692610e142f526dd2
kazi <champlonremii@gmail.com>
2022-04-21 10:23:58 +0200

    account info in component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {ethers} from 'ethers';
 import './App.css';
+import InfosAccount from "./components/InfosAccount";
 
 function App() {
   const [loader, setLoader] = useState(true);
@@ -10,13 +11,13 @@ function App() {
   const [error, setError] = useState('');
 
   useEffect(() => {
-    getAccount();
+    getAccounts();
     setLoader(false);
   }, [])
 
   //little security to check if app realy try to connect to MM
-  window.ethereum.addListener('connect', async(response) => {
-    getAccount();
+  window.ethereum.addListener('connect', async (response) => {
+    getAccounts();
   });
 
   //refresh page if user switch account
@@ -34,27 +35,28 @@ function App() {
     window.location.reload();
   });
 
-  async function getAccount() {
-    if (typeof window.ethereum !== 'undefined')
-    {
-      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts'})
+  async function getAccounts() {
+    if (typeof window.ethereum !== 'undefined') {
+      let accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
       setAccounts(accounts);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      //get the eth balance in WEI of connected account
+      const balance = await provider.getBalance(accounts[0]);
+      // balance WEI -> ETH
+      const balanceInEth = ethers.utils.formatEther(balance)
+      setBalance(balanceInEth);
     }
   }
 
 
-  return (
-    <div className="App">
-      {!loader && accounts.length > 0?
-          <p>Your connected with account : {accounts[0]}</p>
-          :
-          <div>
-            <p>Not connected</p>
-            <button onClick={getAccount}>Connect</button>
-          </div>
-      }
-    </div>
-  );
+    return (
+        <div className="App">
+          <button className="connect_btn" onClick={getAccounts}>Connect Metamask</button>
+          <InfosAccount loader={loader} accounts={accounts} balance={balance}/>
+
+        </div>
+    );
+
 }
 
 export default App;
---
+++ src/components/InfosAccount.js
@@ -0,0 +1,19 @@
+
+function InfosAccount (props) {
+    return (
+        <div>
+            {!props.loader && props.accounts.length > 0 ?
+                <div>
+                    <p>Your connected with account : {props.accounts[0]}</p>
+                    <p>Your total ETH : {props.balance}</p>
+                    <p>{props.balance < 0.3 ? 'Not enougth ETH to be whitelisted' : 'Enougth ETH to be whitelisted'}</p>
+                </div>
+                :
+                <div>
+                    <p>Not connected</p>
+                </div>
+            }
+        </div>
+    )
+}
+export default InfosAccount;
