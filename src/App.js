eddecc112dafba046614e2ca7a01b3ca52389935
kazi <champlonremii@gmail.com>
2022-04-21 09:41:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import {ethers} from 'ethers';
 import './App.css';
 
 function App() {
+  const [loader, setLoader] = useState(true);
+  const [accounts, setAccounts] = useState([]);
+  const [balance, setBalance] = useState();
+  const [succes, setSucces] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getAccount();
+    setLoader(false);
+  }, [])
+
+  //little security to check if app realy try to connect to MM
+  window.ethereum.addListener('connect', async(response) => {
+    getAccount();
+  });
+
+  //refresh page if user switch account
+  window.ethereum.on('accountsChanged', () => {
+    window.location.reload();
+  });
+
+  //refresh page if user switch network
+  window.ethereum.on('chainChanged', () => {
+    window.location.reload();
+  });
+
+  //refresh page if user disconnect
+  window.ethereum.on('disconnect', () => {
+    window.location.reload();
+  });
+
+  async function getAccount() {
+    if (typeof window.ethereum !== 'undefined')
+    {
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts'})
+      setAccounts(accounts);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loader && accounts.length > 0?
+          <p>Your connected with account : {accounts[0]}</p>
+          :
+          <div>
+            <p>Not connected</p>
+            <button onClick={getAccount}>Connect</button>
+          </div>
+      }
     </div>
   );
 }
